74f3c4f61d70962091145f01a2b7307c34046002
Ryan Faulkner <ryan.art.faulkner@gmail.com>
2025-09-17 13:00:26 +0100

    Update launch files.

--- launch_job_api.sh
+++ launch_job_api.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-#SBATCH --account=aip-rgrosse
+#SBATCH --account=def-rgrosse
 #SBATCH --job-name=govsim_elect
 #SBATCH --output=slurm/output/%j_%x.out
 
 #SBATCH --time=0-1:00:00
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:l40s:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
 
-model_id="mistralai/Mistral-7B-Instruct-v0.2"
+# model_id="openrouter-google/gemma-3-27b-it:free"
+model_id="openrouter-google/gemma-3-12b-it:free"
+
 experiment="fish_baseline_concurrent_leaders"
 
-project_dir="/home/$USER/projects/aip-rgrosse/$USER/GovSimElect"
+project_dir="/home/$USER/projects/def-rgrosse/$USER/GovSimElect"
 
 export WANDB_DISABLED=true # Optional, depending on if you want to use WandB
 export HF_HOME="/scratch/$USER/hf_cache"
@@ -24,4 +25,4 @@ module load python/3.11.5 cuda/12.2 gcc arrow
 cd $project_dir
 source .venv/bin/activate
 
-python3 -m simulation.main experiment=$experiment llm.path=$model_id
+python3 -m simulation.main_elect experiment=$experiment llm.path=$model_id
