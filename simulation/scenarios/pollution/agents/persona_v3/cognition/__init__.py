b5b9eb51700db1624b5a0dadcfd056e1971e0be4
rfaulkner <rfaulk@cs.toronto.edu>
2026-02-06 20:06:21 -0500

    Remove pollution and sheep scenarios.

--- simulation/scenarios/pollution/agents/persona_v3/cognition/__init__.py
+++
@@ -1,5 +0,0 @@
-from .act import PollutionActComponent
-from .converse import PollutionConverseComponent
-from .plan import PollutionPlanComponent
-from .reflect import PollutionReflectComponent
-from .store import PollutionStoreComponent
